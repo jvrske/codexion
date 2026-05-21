1d15ea7d6509287a4b55981d6fc4b4520575190c
csilva <jarskewow@hotmail.com>
2026-05-21 11:37:31 +0100

    v0

---
+++ includes.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva <csilva@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 11:24:37 by csilva            #+#    #+#             */
+/*   Updated: 2026/05/21 11:28:22 by csilva           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+// Parser
+void	parser(int argc, char *argv[]);
+
+#endif
